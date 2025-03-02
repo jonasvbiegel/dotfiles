7b1676b5212ef202f92c5293da08cde39595ed25
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-03-02 23:16:51 +0100

    kldsa

--- laptop/nvim/init.lua
+++ laptop/nvim/init.lua
@@ -420,7 +420,7 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			--vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+			--vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 			vim.keymap.set("n", "<C-s>", builtin.buffers, {})
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
---
+++ laptop/nvim/lua/custom/plugins/snacks.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			image = {},
+			explorer = {},
+			picker = {},
+			bufdelete = {},
+		},
+		keys = {
+			{
+				"<C-e>",
+				function()
+					Snacks.explorer()
+				end,
+				desc = "File explorer",
+			},
+			{
+				"<leader>m",
+				function()
+					Snacks.bufdelete()
+				end,
+				desc = "Delete current buffer",
+			},
+		},
+	},
+}
