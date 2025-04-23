2b5b47a9260b47a57ddcf675a08f948eba81e498
Jonas Vittrup Biegel <10519146@ucn.dk>
2025-04-23 08:59:04 +0200

    Removed useless dots

--- desktop/nvim/lua/custom/plugins/vimtex.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"lervag/vimtex",
-	ft = "tex",
-	config = function()
-		vim.g.vimtex_view_method = "zathura"
-		vim.g.vimtex_compiler_engine = "pdflatex"
-		vim.g.vimtex_quickfix_open_on_warning = 0
-	end,
-}
